03280a5162e26fb4f0a66fa15d9df1ad44cf5b1e
Vijay Kedar <54878107+kedarvijay@users.noreply.github.com>
2022-01-26 23:11:40 +0530

    Create IDValidator.java

---
+++ IDValidator.java
@@ -0,0 +1,15 @@
+package com.validation;
+
+public abstract class IDValidator {
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	public abstract boolean validate();
+
+}
